7b101989e5849b7cdd6aea5fd1549a5334ab600b
somya-jangid <somyajangid78@gmail.com>
2025-02-24 01:28:52 +0530

    Add or update frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,1 @@
+import React from 'react'; import Header from './components/Header'; import Footer from './components/Footer'; function App() { return (<div><Header /><main><h1>Welcome to Netflix-Inspired</h1></main><Footer /></div>); } export default App;
